2f52232969866bb3347b3403eee8859ba3872389
Wendell Oliveira <wendelljuliooliveirajr@gmail.com>
2025-02-19 00:05:16 -0500

    Part 4.2 completed

---
+++ Units/Gameplay Mechanics/Prototype 4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+    private GameObject player;
+    public float speed;
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // One object minus the vector of the other object gives the direction from one object to the other
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+        // enemyRb.AddForce((player.transform.position - transform.position).normalized * speed); "cleaner" code above
+    } 
+}
---
+++ Units/Gameplay Mechanics/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
+    void Start()
+    {
+        
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+       
+    }
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+}
